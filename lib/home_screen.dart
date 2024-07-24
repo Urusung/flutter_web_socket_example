13ffd6d2e25e885e09d3bc06d53a51904c228455
최우성 <choiwoosung61@gmail.com>
2024-07-24 18:11:00 +0900

    feat: web_socket_channel 예제 추가

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _AppState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _AppState extends State<HomeScreen> {
+  late final WebSocketChannel _channel;
   late final TextEditingController _controller;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
+    _channel =
+        WebSocketChannel.connect(Uri.parse('wss://echo.websocket.events'));
   }
 
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
+    _channel.sink.close();
   }
 
   @override
@@ -29,15 +34,28 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Flutter Web Socket Example"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _channel.sink.add(_controller.text.toString());
+        },
         child: const Icon(Icons.send),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            const TextField(),
-            Container(),
+            TextField(
+              controller: _controller,
+            ),
+            StreamBuilder(
+              stream: _channel.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data);
+                } else {
+                  return Container();
+                }
+              },
+            )
           ],
         ),
       ),
